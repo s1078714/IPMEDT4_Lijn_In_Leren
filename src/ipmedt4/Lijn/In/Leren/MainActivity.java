79a2d55af450e1a8462dbc22c7de6b19af762230
s1078714 <s1078714@student.hsleiden.nl>
2014-03-17 13:52:49 +0100

    basis opgezet van de app. MainActivity heeft schijnbaar moeilijkheden met R.java, ik weet niet wat hier de oorzaak van is. Dit zal ik later uitzoeken.

--- src/ipmedt4/Lijn/In/Leren/MainActivity.java
+++ src/ipmedt4/Lijn/In/Leren/MainActivity.java
@@ -1,5 +1,41 @@
 package ipmedt4.Lijn.In.Leren;
 
-public class MainActivity {
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+public class MainActivity extends Activity 
+{
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main );
+		
+		populateListView();
+	}
+
+	/**
+	 * Vult de lijst met items.
+	 * @author Gunnar Kelders
+	 * @version 0.1 -- 17 maart 2014
+	 */
+	private void populateListView() {
+		// TODO Auto-generated method stub
+		String[] myItems = {
+				// Hier vind de SQL naar android/java plaats.
+		};
+
+		// Build adapter
+		ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(
+				this,				// Context for the activity
+				R.layout.the_item, 	// Layout to use (create)
+				myItems);			// Items to be displayed
+
+		// Configure the list view
+		ListView list = (ListView) findViewById(R.id.listViewMain);
+		list.setAdapter(myAdapter);
+	}
 }
