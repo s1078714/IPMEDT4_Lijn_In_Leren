aa4907253cbbf61be7fcaef14b2a7ac4b3689bf2
SuperJoot <Joost_van_eeden@hotmail.com>
2014-03-27 15:28:43 +0100

    Joost-"vooral gewerkt aan groepenklasse"

--- src/ipmedt4/Lijn/In/Leren/MainActivity.java
+++ src/ipmedt4/Lijn/In/Leren/MainActivity.java
@@ -1,5 +1,7 @@
 package ipmedt4.Lijn.In.Leren;
 
+
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View.OnClickListener;
@@ -12,7 +14,9 @@ public class MainActivity extends Activity
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main );
+		setContentView(R.layout.main_activity);
+		
+		
 		
 		populateListView();
 	}
