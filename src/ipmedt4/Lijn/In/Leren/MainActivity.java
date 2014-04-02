ba138853a1a05653dd45a2b5b3abd9aa0e73042a
SuperJoot <Joost_van_eeden@hotmail.com>
2014-04-02 16:57:39 +0200

    Dubbele code verwijderd -Joost

--- src/ipmedt4/Lijn/In/Leren/MainActivity.java
+++ src/ipmedt4/Lijn/In/Leren/MainActivity.java
@@ -1,56 +1,7 @@
 package ipmedt4.Lijn.In.Leren;
 
-
-
-import com.example.lijn_in_leren.R;
-
-import android.app.Activity;
-import android.os.Bundle;
-import android.view.View.OnClickListener;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
-
-public class MainActivity extends Activity 
-{
-	@Override
-	protected void onCreate(Bundle savedInstanceState)
-	{
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		
-		ListView listView = (ListView) findViewById(R.id.list);
-		
-		populateListView();
-	}
-
-	/**
-	 * Vult de lijst met items.
-	 * @author Gunnar Kelders
-	 * @version 0.1 -- 17 maart 2014
-	 */
-	private void populateListView() {
-		// TODO Auto-generated method stub
-		String[] myItems = {
-				// Hier vind de SQL naar android/java plaats.
-		};
-
-		// Build adapter
-		ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(
-				this,				// Context for the activity
-				R.layout.the_item, 	// Layout to use (create)
-				myItems);			// Items to be displayed
-
-		// Configure the list view
-		ListView list = (ListView) findViewById(R.id.listViewMain);
-		list.setAdapter(myAdapter);
-	}
-}
-package ipmedt4.Lijn.In.Leren;
-
 import java.util.List;
 
-import de.vogella.android.sqlite.first.Comment;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View.OnClickListener;
