354a0a022b0b0b3052915bb7710dc38a98eeca23
SuperJoot <Joost_van_eeden@hotmail.com>
2014-04-02 17:04:11 +0200

    activity main omgedraaid

--- src/ipmedt4/Lijn/In/Leren/MainActivity.java
+++ src/ipmedt4/Lijn/In/Leren/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends Activity
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main );
+		setContentView(R.layout.main_activity);
 		
 		populateListView();
 		
