5257c3feb41b6a526b85afe5a9a8290a68318aee
s1078714 <s1078714@student.hsleiden.nl>
2014-04-02 15:40:34 +0200

    typfoutje R.layout.activity_main aangepast naar R.layout.main_activity

--- src/ipmedt4/Lijn/In/Leren/Groepen.java
+++ src/ipmedt4/Lijn/In/Leren/Groepen.java
@@ -15,7 +15,7 @@ public class Groepen extends ListActivity
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main );
+		setContentView(R.layout.main_activity );
 		
 		populateListView();
 	}
